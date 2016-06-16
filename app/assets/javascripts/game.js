b80422cecd7b5c2df62376cedad4a837b579cd82
Henry Doan <henrydoan96@gmail.com>
2016-06-16 16:00:39 -0600

    unfinshed

---
+++ app/assets/javascripts/game.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+	 var hide = $('.hidden')
+
+
+	$('.hidden').click(function() {
+		$(this).toggleClass('hidden');
+	});
+
+	function hidden() {
+
+	}
+
+	function unhidden(){
+		hide.toggleClass('hidden');
+	}
+
+	function comparision() {
+		if ()
+			//leave card unflip
+			//CANT Flip it
+		else // flip card
+	}
+
+});
